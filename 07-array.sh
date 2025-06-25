179b88dfbcd61ddbf21de1be54d55d7719f3af3c
Garimidi Dhana Lakshmi <dhana4133@gmail.com>
2025-06-25 17:29:17 +0530

    array created

--- 07-array.sh
+++ 07-array.sh
@@ -4,5 +4,7 @@ MOVIES=("Rakhi" "Simha" "Godavati" "Court")
 
 echo "First Movie: ${MOVIES[0]}"
 echo "First Movie: ${MOVIES[3]}"
+echo "First Movie: ${MOVIES[4]}"
+
 
 echo "all Movies:  ${MOVIES[@]}"
