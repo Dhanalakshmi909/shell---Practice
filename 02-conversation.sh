a75969fa31cfaabb625dd29e5784a8a2b65ce66e
Garimidi Dhana Lakshmi <dhana4133@gmail.com>
2025-06-25 11:47:30 +0530

    conversation started

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 
-echo Dhana:Hey rama,how are you?
-echo rama:hi Dhana, Iam good, how are you doing.
-echo Dhana: Everything is good, what about you?
-echo rama: I have some problems, can you come with me.
-echo Dhana: I have urgent meeting, you can carry on I will be join
-echo rama: okay bye!
-echo Dhana: bye!
+echo "Dhana:: Hey rama,how are you?"
+echo "rama:: hi Dhana, Iam good, how are you doing."
+echo "Dhana:: Everything is good, what about you?"
+echo "rama::  I have some problems, can you come with me."
+echo "Dhana:: I have urgent meeting, you can carry on I will be join"
+echo "rama::  okay bye!"
+echo "Dhana:: bye!"
