a5f38fa4497d796b09bdba21a72a59286329cbec
Garimidi Dhana Lakshmi <dhana4133@gmail.com>
2025-06-25 13:25:19 +0530

    variables

---
+++ 04-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+echo "$$1:: Hey,$$2 how are you?"
+echo "$$2:: hi $$1, Iam good, how are you doing."
+echo "$$1:: Everything is good, what about you?"
+echo "$$2::  I have some problems , can you come with me."
+echo "$$1:: I have urgent meeting, you can carry on I will be join"
+echo "$$2::  okay bye!"
+ 
+
