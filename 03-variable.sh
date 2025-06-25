c2e056d434b5bb33f1fcfc7abb12bc0e32370f4c
Garimidi Dhana Lakshmi <dhana4133@gmail.com>
2025-06-25 13:01:47 +0530

    conversation started

--- 03-variable.sh
+++ 03-variable.sh
@@ -1,22 +1,13 @@
-# !# /bin/bash
-
-# PERSON1 = Dhana
-# PERSON2 = rama
-# echo "$PERSON1:: Hey rama,how are you?"
-# echo "rama:: hi Dhana, Iam good, how are you doing."
-# echo "Dhana:: Everything is good, what about you?"
-# echo "rama::  I have some problems Dhana, can you come with me."
-# echo "Dhana:: I have urgent meeting, you can carry on I will be join"
-# echo "rama::  okay bye!"
-
-
 #!/bin/bash
 
 PERSON1=Dhana
-PERSON2=Rama
-echo "$PERSON1:: Hey $PERSON2,how are you?"
+PERSON2=rama
+echo "$PERSON1:: Hey,$PERSON2 how are you?"
 echo "$PERSON2:: hi $PERSON1, Iam good, how are you doing."
 echo "$PERSON1:: Everything is good, what about you?"
-echo "$PERSON2::  I have some problems $PERSON1, can you come with me."
+echo "$PERSON2::  I have some problems , can you come with me."
 echo "$PERSON1:: I have urgent meeting, you can carry on I will be join"
 echo "$PERSON2::  okay bye!"
+
+
+
