4ca14012523b26620933876c8654844515d49934
Garimidi Dhana Lakshmi <dhana4133@gmail.com>
2025-06-25 13:04:36 +0530

    variables

--- 03-variable.sh
+++ 03-variable.sh
@@ -8,6 +8,6 @@ echo "$PERSON1:: Everything is good, what about you?"
 echo "$PERSON2::  I have some problems , can you come with me."
 echo "$PERSON1:: I have urgent meeting, you can carry on I will be join"
 echo "$PERSON2::  okay bye!"
-
+ 
 
 
