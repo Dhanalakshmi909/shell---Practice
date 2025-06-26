6d6f4c890b37f655163639c3bc0610d8d22824ff
Garimidi Dhana Lakshmi <dhana4133@gmail.com>
2025-06-26 15:35:26 +0530

    special variables created

--- 08-specialvariables.sh
+++ 08-specialvariables.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-echo "all variables passed to the script: $@"
-echo "number variables passed to the script: $#"
+echo "All variables passed to the script: $@"
+echo "Number variables passed to the script: $#"
+echo "Script name: $0"
+echo "Current working directory: $PWD"
+echo "User running the script: $USER"
+echo "Home directory user running the script: $HOME"
+echo "PID of the script: $$"
+sleep 10 &
+echo "PID of last command in back ground: $!"                  
