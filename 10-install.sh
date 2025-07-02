a76f91706b8a4247b167523c77ff718ffbff5c6a
Garimidi Dhana Lakshmi <dhana4133@gmail.com>
2025-07-02 11:59:38 +0530

    installcreated

--- 10-install.sh
+++ 10-install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-USERID=(id -u)
+USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then 
  echo "ERROR:: "Please run this script with root access"
 else
  echo "you are running with root access"
+
 fi
 
-# dnf install mysql -y
+ dnf install mysql -y
+
