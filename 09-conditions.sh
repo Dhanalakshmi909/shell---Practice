0a7b096d4170bae69a45436d21038eb19b4877e4
Garimidi Dhana Lakshmi <dhana4133@gmail.com>
2025-07-02 08:57:06 +0530

    conditions created

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -2,7 +2,7 @@
 
 Number=$1
 
-if [ $Number -lt 10 ]
+if  [$Number -lt 10]
 then
   echo "Given number $Number is lessthan 10"
 else
@@ -10,16 +10,3 @@ else
 fi
 
 
-# NUMBER=$1
-
-# # -gt --> greater than
-# # -lt --> less than
-# # -eq --> equal
-# # -ne --> not equal
-
-# if [ $NUMBER -lt 10 ]
-# then
-#     echo "Given number $NUMBER is less than 10"
-# else
-#     echo "Given number $NUMBER is not less than 10"
-# fi
