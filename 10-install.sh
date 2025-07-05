53c94cbe3432d5c744f950cec857dd093c5df991
Garimidi Dhana Lakshmi <dhana4133@gmail.com>
2025-07-05 10:35:47 +0530

    Install created

--- 10-install.sh
+++ 10-install.sh
@@ -5,7 +5,7 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then 
    echo "ERROR:: Please run this script with root access"
-   exit 1 #give other than 0 upto 127
+   #exit 1 #give other than 0 upto 127
 else
    echo "you are running with root access"
  
@@ -23,7 +23,7 @@ then
   echo "Install the mysql is ...success"
 else
   echo "Install the mysql is ...failure"
-  exit 1
+  #exit 1
 fi
 else
  echo "mysql already installed...Nothing to do"
